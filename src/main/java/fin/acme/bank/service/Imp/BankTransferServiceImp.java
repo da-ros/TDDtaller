115561349a8fc5d65b0d657a8b5c820f4999c4a5
Giovanny Jácome <gio_jacome@hotmail.com>
2024-12-27 11:14:41 -0500

    test:Transfer desc:whenEnoughAmountThenTransferSuccess  phase:sucess  result: passed

--- src/main/java/fin/acme/bank/service/Imp/BankTransferServiceImp.java
+++ src/main/java/fin/acme/bank/service/Imp/BankTransferServiceImp.java
@@ -9,8 +9,22 @@ public class BankTransferServiceImp implements BankTransferService {
     private BankTransferServiceImp bankTransferServiceImp;
     private Map<String, Account> accountData;
 
-    public boolean transfer(String fromAccount, String toAccount, double amount, String description) {
-        return false;
+    public boolean transfer(String fromAccountNumber, String toAccountNumber, double amount, String description) {
+        if (!accountData.containsKey(fromAccountNumber) || !accountData.containsKey(toAccountNumber)) {
+            return false;
+        }
+
+        Account fromAccount = accountData.get(fromAccountNumber);
+        Account toAccount = accountData.get(toAccountNumber);
+
+        if (fromAccount.getBalance() < amount) {
+            return false; // Fondos insuficientes
+        }
+
+        fromAccount.setBalance(fromAccount.getBalance() - amount);
+        toAccount.setBalance(toAccount.getBalance() + amount);
+
+        return true;
     }
 
     public void  setAccountData(Map<String, Account> accountData) {
